2fb551d7beebc0f96bd931cf57a9b1fcaae6cbad
Gabrielbemo <gabrielbemoura@gmail.com>
2018-08-16 09:35:52 -0300

    adicionando imagens ao bando de dados mongodb

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var multiparty = require('connect-multiparty');
 var mongodb = require('mongodb');
 var objectId = require('mongodb').ObjectID;
+var fs = require('fs');
 
 var app = express();
 
@@ -30,26 +31,43 @@ app.get('/', function (req, res) {
 //POST (create)
 app.post('/api', function (req, res) {
 
-    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader("Access-Control-Allow-Origin", "*");
 
-    var dados = req.body;
+    var date = new Date();
+    var time_stamp = date.getTime();
 
-    console.log(dados);
+    var url_imagem = time_stamp + '_' + req.files.arquivo.originalFilename;
 
-    /*
-    db.open(function (err, mongoclient) {
-        mongoclient.collection('postagens', function (err, collection) {
-            collection.insert(dados, function (err, records) {
-                if (err) {
-                    res.json({ 'status': 'erro' });
-                } else {
-                    res.json({ 'status': 'sucesso na inclusão' });
-                }
-                mongoclient.close();
+    var path_origem = req.files.arquivo.path;
+    var path_destino = './uploads/' + url_imagem;
+
+    fs.rename(path_origem, path_destino, function (err) {
+        if (err) {
+            res.status(500).json({ error: err });
+            return;
+        }
+
+        var dados = {
+            url_imagem: url_imagem,
+            titulo: req.body.titulo
+        }
+
+        
+        db.open(function (err, mongoclient) {
+            mongoclient.collection('postagens', function (err, collection) {
+                collection.insert(dados, function (err, records) {
+                    if (err) {
+                        res.json({ 'status': 'erro' });
+                    } else {
+                        res.json({ 'status': 'sucesso na inclusão' });
+                    }
+                    mongoclient.close();
+                });
             });
         });
-    });
-    */
+        
+
+    })
 });
 
 //GET (ready)
@@ -92,10 +110,10 @@ app.put('/api/:id', function (req, res) {
                 { _id: objectId(req.params.id) },
                 { $set: { titulo: req.body.titulo } },
                 {},
-                function(err, records){
-                    if(err) {
+                function (err, records) {
+                    if (err) {
                         res.json(err);
-                    }else{
+                    } else {
                         res.json(records);
                     }
                     mongoclient.close();
@@ -104,17 +122,17 @@ app.put('/api/:id', function (req, res) {
         });
     });
 });
- 
+
 //DELETE by ID (update)
 app.delete('/api/:id', function (req, res) {
     db.open(function (err, mongoclient) {
         mongoclient.collection('postagens', function (err, collection) {
             collection.remove(
-                {_id:objectId(req.params.id)},
-                function(err,records){
-                    if(err){
+                { _id: objectId(req.params.id) },
+                function (err, records) {
+                    if (err) {
                         res.json(err);
-                    }else{
+                    } else {
                         res.json(records);
                     }
                     mongoclient.close();
